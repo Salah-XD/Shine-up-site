0476621721961a3d3a1c5eb7d42fe9380a877343
LokeshNivash <nivashnisa@gmail.com>
2024-10-25 19:55:54 +0530

    new

---
+++ src/constants/index.jsx
@@ -0,0 +1,173 @@
+import { ScissorsLineDashed } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { PanelTop } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
+import { GlobeLock } from "lucide-react";
+
+
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+import user3 from "../assets/profile-pictures/user3.jpg";
+import user4 from "../assets/profile-pictures/user4.jpg";
+import user5 from "../assets/profile-pictures/user5.jpg";
+import user6 from "../assets/profile-pictures/user6.jpg";
+
+export const navItems = [
+  { label: "Services", href: "#" },
+  { label: "Portfolio", href: "#" },
+  { label: "Testimonials", href: "#" },
+  { label: "Team", href: "#" }
+];
+
+export const testimonials = [
+  {
+    user: "John Doe",
+    company: "Stellar Solutions",
+    image: user1,
+    text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
+  },
+  {
+    user: "Jane Smith",
+    company: "Blue Horizon Technologies",
+    image: user2,
+    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+  },
+  {
+    user: "David Johnson",
+    company: "Quantum Innovations",
+    image: user3,
+    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+  },
+  {
+    user: "Ronee Brown",
+    company: "Fusion Dynamics",
+    image: user4,
+    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+  },
+  {
+    user: "Michael Wilson",
+    company: "Visionary Creations",
+    image: user5,
+    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+  },
+  {
+    user: "Emily Davis",
+    company: "Synergy Systems",
+    image: user6,
+    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+  },
+];
+
+export const features = [
+  {
+    icon: <ScissorsLineDashed />,
+    text: "Video Editing",
+    description:
+      "Edit like a pro and make your brand shine with our comprehensive video editing services.",
+  },
+  {
+    icon: <PanelTop />,
+    text: "Web Development",
+    description:
+      "Build responsive and engaging websites with ShineUp’s expert web development services.",
+  },
+  {
+    icon: <ShieldHalf />,
+    text: "SEO",
+    description:
+      "Rank higher and attract more customers with ShineUp’s comprehensive SEO services, focusing on best practices that deliver measurable results and maximize your online presence.",
+  },
+  {
+    icon: <BatteryCharging />,
+    text: "Graphic Design",
+    description:
+      "Transform ideas into visual masterpieces with ShineUp’s expert graphic design solutions, tailored to reflect your brand’s unique personality and resonate with your target audience.",
+  },
+  {
+    icon: <PlugZap />,
+    text: "3D Modeling",
+    description:
+      "Bring your concepts to life with ShineUp’s advanced 3D modeling services, where we turn your ideas into detailed and realistic visual representations for projects of any scale.",
+  }
+];
+
+export const checklistItems = [
+  {
+    title: "Code merge made easy",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Review code without worry",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "AI Assistance to reduce time",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Share work in minutes",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+];
+
+export const pricingOptions = [
+  {
+    title: "Free",
+    price: "$0",
+    features: [
+      "Private board sharing",
+      "5 Gb Storage",
+      "Web Analytics",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Pro",
+    price: "$10",
+    features: [
+      "Private board sharing",
+      "10 Gb Storage",
+      "Web Analytics (Advance)",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: "$200",
+    features: [
+      "Private board sharing",
+      "Unlimited Storage",
+      "High Performance Network",
+      "Private Mode",
+    ],
+  },
+];
+
+export const resourcesLinks = [
+  { href: "#", text: "Getting Started" },
+  { href: "#", text: "Documentation" },
+  { href: "#", text: "Tutorials" },
+  { href: "#", text: "API Reference" },
+  { href: "#", text: "Community Forums" },
+];
+
+export const platformLinks = [
+  { href: "#", text: "Features" },
+  { href: "#", text: "Supported Devices" },
+  { href: "#", text: "System Requirements" },
+  { href: "#", text: "Downloads" },
+  { href: "#", text: "Release Notes" },
+];
+
+export const communityLinks = [
+  { href: "#", text: "Events" },
+  { href: "#", text: "Meetups" },
+  { href: "#", text: "Conferences" },
+  { href: "#", text: "Hackathons" },
+  { href: "#", text: "Jobs" },
+];
