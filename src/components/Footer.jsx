18184e6beeac987d76b71d97b0cd7091a1865b49
LokeshNivash <nivashnisa@gmail.com>
2024-10-27 13:49:53 +0530

    seo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,10 @@ const Footer = () => {
     return (
         <section className="footer py-10">
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-                <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-y-16 gap-x-12">
+                    {/* Logo and Social Media Links */}
                     <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-                        {/* Parent div for logo and icons */}
                         <div className="flex flex-col items-center mb-4">
-                            {/* Logo Image with Link to Home */}
                             <a href="/" title="Home">
                                 <img 
                                     className="w-auto h-40 mb-2" 
@@ -18,7 +17,6 @@ const Footer = () => {
                                     alt="Logo"
                                 />
                             </a>
-
                             {/* Social Media Links */}
                             <ul className="flex items-center justify-center space-x-4">
                                 <li>
@@ -69,64 +67,15 @@ const Footer = () => {
                             </ul>
                         </div>
                     </div>
-                    
-                    {/* Contact Form with Netlify Integration */}
-                    <div className="col-span-2 md:col-span-3 lg:col-span-4">
-                        <h4 className="text-3xl font-semibold mb-7">Contact Us</h4>
-                        <form 
-                            name="contact" 
-                            method="POST" 
-                            data-netlify="true"
-                            className="subscribe-form flex flex-col space-y-4"
+
+                    {/* Contact Us Button */}
+                    <div className="col-span-2 md:col-span-3 lg:col-span-2 flex items-center justify-center">
+                        <a
+                            href="mailto:digitalshineup@gmail.com"
+                            className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-4 rounded-md text-white text-center"
                         >
-                            <input type="hidden" name="form-name" value="contact" />
-
-                            {/* Name Field */}
-                            <div>
-                              <label className="text-white mb-1" htmlFor="name">Name</label>
-                                <input 
-                                    type="text" 
-                                    name="name" 
-                                    placeholder="Enter your name" 
-                                    className="input-field w-full p-3 rounded-md" 
-                                    required 
-                                />
-                            </div>
-
-                            {/* Email Field */}
-                            <div>
-                              <label className="text-white mb-1" htmlFor="email">Email</label>
-                                <input 
-                                    type="email" 
-                                    name="email" 
-                                    placeholder="Enter your email" 
-                                    className="input-field w-full p-3 rounded-md" 
-                                    required 
-                                />
-                            </div>
-
-                            {/* Message Field */}
-                            <div>
-                              <label className="text-white mb-1" htmlFor="email">Email</label>
-                                <textarea 
-                                    name="details" 
-                                    placeholder="Enter your message" 
-                                    className="input-field w-full p-3 rounded-md" 
-                                    rows="4" 
-                                    required
-                                />
-                            </div>
-
-                            {/* Centered Button */}
-                            <div className="text-center">
-                                <button 
-                                    type="submit" 
-                                    className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-4 rounded-md text-white mx-auto"
-                                >
-                                    Send
-                                </button>
-                            </div>
-                        </form>
+                            Contact Us
+                        </a>
                     </div>
                 </div>
             </div>
