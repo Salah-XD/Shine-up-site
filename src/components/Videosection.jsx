27a081b0166d7019514a1be4d67de796c6b38b30
Salah-XD <hudeensala948@gmail.com>
2024-11-09 01:26:23 +0530

    feat:gsap

--- src/components/Videosection.jsx
+++ src/components/Videosection.jsx
@@ -1,24 +1,23 @@
 const Videosection = () => {
-    const handleContextMenu = (e) => {
-      e.preventDefault();
-    };
-  
-    return (
-      <div className="relative">
-        <video
-          src="https://res.cloudinary.com/dwlukj2fe/video/upload/v1731065583/wr1iujbrmwlusfdxexaw.webm"
-          type="video/webm"
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="w-full h-auto object-cover -mt-10 md:h-[80vh]"
-          style={{ height: "100vh" }}
-          onContextMenu={handleContextMenu}
-        />
-      </div>
-    );
+  const handleContextMenu = (e) => {
+    e.preventDefault();
   };
-  
-  export default Videosection;
-  
+
+  return (
+    <div className="relative">
+      <video
+        src="https://res.cloudinary.com/dwlukj2fe/video/upload/v1731065583/wr1iujbrmwlusfdxexaw.webm"
+        type="video/webm"
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="w-full h-auto object-cover -mt-10 md:h-[80vh]"
+        style={{ height: "100vh" }}
+        onContextMenu={handleContextMenu}
+      />
+    </div>
+  );
+};
+
+export default Videosection;
